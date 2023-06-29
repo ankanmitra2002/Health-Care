149715abd258e4c17f4c936ae2bcce9ad3c7decc
Soutik Mondal <soutikmondaljee100@gmail.com>
2023-06-29 11:15:42 +0530

    hello

---
+++ ChatGPT_flutter_course-master/lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
--- android/app/src/main/kotlin/com/health/healthcare/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.health.healthcare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
